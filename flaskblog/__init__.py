1ecb66d8f0f96ce73e083b74ef9d3502534a8426
limlleonard <limlleonard@gmail.com>
2024-11-01 10:49:58 +0100

    env and gitignore

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,13 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret' #'5791628bb0b13ce0c676dfde280ba245'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# this will create a instance folder and place .db there
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../flaskblog/site.db'
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://uwqvstqhzwieuecz7zwg:RJXe2Ge80Sw5rehSPrHHDHFn6FGyMF@bvusyauiyibht5nvqff6-postgresql.services.clever-cloud.com:50013/bvusyauiyibht5nvqff6'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
