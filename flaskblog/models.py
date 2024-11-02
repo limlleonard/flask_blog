a4fe39a62b5717d788219b5e2f32af44419536d1
limlleonard <limlleonard@gmail.com>
2024-11-02 10:33:23 +0100

    add new column topic

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -25,6 +25,7 @@ class Post(db.Model):
     # __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
+    topic = db.Column(db.String(20), nullable=False, default='default')
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('benutzer.id'), nullable=False)
